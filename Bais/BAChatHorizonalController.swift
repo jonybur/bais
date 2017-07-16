e5c64f05142646799597a755393edf5562154392
Jonathan Bursztyn <jonathan.bursztyn@mulesoft.com>
2017-07-16 01:11:47 -0300

    Minor changes after Swift 4 rollback

--- Bais/BAChatHorizonalController.swift
+++ Bais/BAChatHorizonalController.swift
@@ -14,6 +14,7 @@ class BAChatHorizonalController: UIViewController, ASCollectionDataSource, ASCol
     
     let collectionNode: ASCollectionNode!
     var sessions = [Session]()
+    var superNavigationController: UINavigationController?
     
     required init(with sessions: [Session]) {
         
@@ -47,6 +48,11 @@ class BAChatHorizonalController: UIViewController, ASCollectionDataSource, ASCol
     
     override func viewWillLayoutSubviews() {
         collectionNode.frame = self.view.bounds
+    }
+    
+    func collectionNode(_ collectionNode: ASCollectionNode, didSelectItemAt indexPath: IndexPath) {
+        let session = sessions[indexPath.item]
+        superNavigationController?.pushViewController(BAChatController(with: session), animated: true)
     }
     
     func collectionNode(_ collectionNode: ASCollectionNode, nodeForItemAt indexPath: IndexPath) -> ASCellNode {
